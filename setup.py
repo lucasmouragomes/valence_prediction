0e402ebf3c04f00b19daaa1601cefb56c5733207
Lucas Moura Gomes <lucasmouragomes@outlook.com>
2023-03-28 19:57:58 -0300

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Valence is an index of mood conveyed by music, used for playlist and song recommendation.',
+    author='lucasmouragomes',
+    license='MIT',
+)
